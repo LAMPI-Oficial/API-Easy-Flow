26b264005179132ee9284e062aead7bb642d7048
Mateus Farias Costa <mateusfariasz86@gmail.com>
2023-03-24 17:39:58 -0300

    Finalizando os metodos de CRUD da entidade event, e implementando metodos para busca por data e horario de eventos

--- src/main/java/br/com/ifce/easyflow/controller/EventController.java
+++ src/main/java/br/com/ifce/easyflow/controller/EventController.java
@@ -1,14 +1,14 @@
 package br.com.ifce.easyflow.controller;
 
+import br.com.ifce.easyflow.controller.dto.event.EventRequestDTO;
 import br.com.ifce.easyflow.controller.dto.event.EventResponseDTO;
+import br.com.ifce.easyflow.model.Event;
 import br.com.ifce.easyflow.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/event")
@@ -16,7 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
     private final EventService eventService;
     @GetMapping
-    public ResponseEntity<Page<EventResponseDTO>> listAll(Pageable pageable){
+    public ResponseEntity<Page<Event>> listAll(Pageable pageable){
         return ResponseEntity.ok(eventService.listAll(pageable));
+    }
+
+    @GetMapping("/find-by-date")
+    public ResponseEntity<Page<Event>> listByDate(@RequestParam(name = "date") String date, Pageable pageable){
+        return ResponseEntity.ok(eventService.listByDate(date,pageable));
+    }
+
+    @GetMapping("/find-by-date-and-time")
+    public ResponseEntity<Page<Event>> listByDateTime(@RequestParam(name = "date") String date, @RequestParam(name = "time") String time, Pageable pageable){
+        return ResponseEntity.ok(eventService.listByDateTime(date, time, pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> listById(@PathVariable Long id){
+        return ResponseEntity.ok(eventService.listById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<EventResponseDTO> save(@RequestBody EventRequestDTO eventRequestSaveDTO){
+        return ResponseEntity.ok(eventService.save(eventRequestSaveDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> update(@PathVariable Long id ,@RequestBody EventRequestDTO eventRequestsDTO){
+        return ResponseEntity.ok(eventService.update(id, eventRequestsDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        eventService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
