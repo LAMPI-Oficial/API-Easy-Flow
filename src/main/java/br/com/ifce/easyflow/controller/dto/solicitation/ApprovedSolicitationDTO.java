2740126fc68f3187c95d560f06b06a4dc4f085e0
Lucas Farias <lucasfariasti0101@gmail.com>
2023-04-10 18:15:24 -0300

    Adição da documentação dos endpoints relacionados a solicitação de equipamentos

--- src/main/java/br/com/ifce/easyflow/controller/dto/solicitation/ApprovedSolicitationDTO.java
+++ src/main/java/br/com/ifce/easyflow/controller/dto/solicitation/ApprovedSolicitationDTO.java
@@ -1,6 +1,7 @@
 package br.com.ifce.easyflow.controller.dto.solicitation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,8 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class ApprovedSolicitationDTO {
 
+    @ApiModelProperty(value = "Equipment ID to be linked to the request",
+            example = "1")
     @JsonProperty(value = "equipment-id")
     @NotBlank(message = "The equipment cannot be empty or null")
     private Long equipmentId;
