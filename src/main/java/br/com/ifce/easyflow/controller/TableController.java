56ce54aff0bfa850627d218b79de65df9c70a0dc
Lucas Farias <lucasfariasti0101@gmail.com>
2023-04-10 21:38:56 -0300

    Adição da documentação dos endpoints relacionados a solicitação de horário

--- src/main/java/br/com/ifce/easyflow/controller/TableController.java
+++ src/main/java/br/com/ifce/easyflow/controller/TableController.java
@@ -5,6 +5,9 @@ import br.com.ifce.easyflow.controller.dto.table.SearchTablesAvailableRequestDTO
 import br.com.ifce.easyflow.controller.dto.table.TablePostRequestDTO;
 import br.com.ifce.easyflow.model.LabTable;
 import br.com.ifce.easyflow.service.TableService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,21 +25,42 @@ public class TableController {
 
     private final TableService tableService;
 
+    @ApiOperation(value = "Returns a list of tables",tags = {"Tables"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful request"),
+    })
     @GetMapping
     public ResponseEntity<List<LabTable>> findAll() {
         return ResponseEntity.ok(tableService.findAll());
     }
 
+    @ApiOperation(value = "Returns a table by id",tags = {"Tables"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful request"),
+            @ApiResponse(code = 404, message = "Table not found in database"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<LabTable> findById(@PathVariable Long id) {
         return ResponseEntity.ok(tableService.findById(id));
     }
 
+    @ApiOperation(value = "Returns a list of available tables on the specified day and shift.",
+            notes = "Submit the day of the week and shift to return tables that are not tied to a time reservation.",
+            tags = {"Tables"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful request"),
+    })
     @GetMapping("/available")
     public ResponseEntity<List<LabTable>> findAvailableTables(@RequestBody @Valid SearchTablesAvailableRequestDTO requestDTO) {
         return ResponseEntity.ok(tableService.tablesAvailable(requestDTO));
     }
 
+    @ApiOperation(value = "Save a table",
+            tags = {"Tables"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful request"),
+            @ApiResponse(code = 409, message = "A table has already been registered with that number."),
+    })
     @PostMapping
     public ResponseEntity<LabTable> save(@RequestBody @Valid TablePostRequestDTO requestDTO, UriComponentsBuilder uriBuilder) {
         LabTable table = tableService.save(requestDTO);
@@ -44,12 +68,25 @@ public class TableController {
         return ResponseEntity.created(uri).body(table);
     }
 
+    @ApiOperation(value = "Update table",
+            tags = {"Tables"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful request"),
+            @ApiResponse(code = 404, message = "Table not found in database"),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<LabTable> update(@PathVariable Long id, @RequestBody @Valid LabTableUpdateRequestDTO requestDTO) {
         LabTable table = tableService.update(id, requestDTO);
         return ResponseEntity.ok(table);
     }
 
+    @ApiOperation(value = "Delete table by id",
+            tags = {"Tables"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful request"),
+            @ApiResponse(code = 409, message = "The table cannot be excluded because it is linked to times already reserved."),
+            @ApiResponse(code = 404, message = "Table not found in database"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         tableService.delete(id);
--- src/main/java/br/com/ifce/easyflow/controller/dto/table/SearchTablesAvailableRequestDTO.java
+++ src/main/java/br/com/ifce/easyflow/controller/dto/table/SearchTablesAvailableRequestDTO.java
@@ -1,6 +1,17 @@
 package br.com.ifce.easyflow.controller.dto.table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotBlank;
 
-public record SearchTablesAvailableRequestDTO(@NotBlank String shiftSchedule, @NotBlank String day) {
+public record SearchTablesAvailableRequestDTO(
+        @ApiModelProperty(value = "Day shift",
+        example = "Morning")
+        @JsonProperty(value = "shift-schedule")
+        @NotBlank String shiftSchedule,
+        @ApiModelProperty(value = "Weekday",
+        example = "Monday")
+        @JsonProperty(value = "day")
+        @NotBlank String day) {
 }
