82e18c394d0e02ed0ec1c375d663b454e33f3ae5
Lucas Farias <lucasfariasti0101@gmail.com>
2023-05-17 18:32:50 -0300

    Atualização dos testes com a utilização do Mockito

--- src/test/java/br/com/ifce/easyflow/service/equipment/EquipmentServiceTest.java
+++ src/test/java/br/com/ifce/easyflow/service/equipment/EquipmentServiceTest.java
@@ -5,59 +5,55 @@ import br.com.ifce.easyflow.controller.dto.equipment.EquipmentPutRequestDTO;
 import br.com.ifce.easyflow.controller.dto.equipment.EquipmentStatusPatchRequestDTO;
 import br.com.ifce.easyflow.model.Equipment;
 import br.com.ifce.easyflow.model.enums.EquipmentAvailabilityStatus;
+import br.com.ifce.easyflow.repository.EquipmentRepository;
 import br.com.ifce.easyflow.service.EquipmentService;
 import br.com.ifce.easyflow.service.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
+import static org.mockito.Mockito.*;
+
 class EquipmentServiceTest {
 
-    @Autowired
+    @InjectMocks
     private EquipmentService equipmentService;
+
+    @Mock
+    private EquipmentRepository equipmentRepository;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void findAll_Equipments_WhenSuccessful() {
-
         PageRequest pageable = PageRequest.of(0, 5);
+        List<Equipment> equipmentList = List.of(createEquipment());
+        PageImpl<Equipment> equipmentPage = new PageImpl<>(equipmentList);
 
-        EquipmentPostRequestDTO equipment1 = EquipmentPostRequestDTO.builder()
-                .name("Aspire-3")
-                .brand("ACER")
-                .processor("Intel i5")
-                .ramMemory("8 gb")
-                .storageMemory("500 mb ssd")
-                .tombo("125478")
-                .build();
+        when(equipmentRepository.findAll(pageable)).thenReturn(equipmentPage);
 
-        EquipmentPostRequestDTO equipment2 = EquipmentPostRequestDTO.builder()
-                .name("ThinkPad - V14")
-                .brand("Lenovo")
-                .processor("Intel i5")
-                .ramMemory("16 gb")
-                .storageMemory("500 mb ssd")
-                .tombo("254876")
-                .build();
+        Page<Equipment> all = equipmentService.findAll(pageable);
+        List<Equipment> equipmentsSavedList = all.stream().toList();
 
-        equipmentService.save(equipment1);
-        equipmentService.save(equipment2);
 
-        Page<Equipment> equipmentsPage = equipmentService.findAll(pageable);
-        List<Equipment> list = equipmentsPage.stream().toList();
+        Assertions.assertFalse(equipmentsSavedList.isEmpty());
+        Assertions.assertEquals(equipmentList.get(0).getName(), equipmentsSavedList.get(0).getName());
+        Assertions.assertNotNull(equipmentsSavedList.get(0).getId());
 
-        Assertions.assertEquals(equipment1.getName(), list.get(0).getName());
-        Assertions.assertNotNull(list.get(0).getId());
-        Assertions.assertEquals(2, list.size());
+        verify(equipmentRepository).findAll(pageable);
 
     }
 
@@ -65,24 +61,23 @@ class EquipmentServiceTest {
     void findAll_Equipments_Returns_EmptyPage_WhenSuccessful() {
 
         PageRequest pageable = PageRequest.of(0, 5);
+        List<Equipment> equipmentList = new ArrayList<>();
+        PageImpl<Equipment> equipmentPage = new PageImpl<>(equipmentList);
 
-        Page<Equipment> equipmentsPage = equipmentService.findAll(pageable);
+        when(equipmentRepository.findAll(pageable)).thenReturn(equipmentPage);
 
-        Assertions.assertTrue(equipmentsPage.isEmpty());
+        Page<Equipment> all = equipmentService.findAll(pageable);
+
+        Assertions.assertTrue(all.isEmpty());
+        verify(equipmentRepository).findAll(pageable);
+        verifyNoMoreInteractions(equipmentRepository);
     }
 
     @Test
     void findById_Return_Equipment_WhenSuccessful() {
-        EquipmentPostRequestDTO equipment = EquipmentPostRequestDTO.builder()
-                .name("Aspire-3")
-                .brand("ACER")
-                .processor("Intel i5")
-                .ramMemory("8 gb")
-                .storageMemory("500 mb ssd")
-                .tombo("125478")
-                .build();
+        Equipment equipment = createEquipment();
 
-        equipmentService.save(equipment);
+        when(equipmentRepository.findById(anyLong())).thenReturn(Optional.of(equipment));
 
         Equipment equipmentSaved = equipmentService.findById(1L);
 
@@ -93,6 +88,8 @@ class EquipmentServiceTest {
 
     @Test
     void findById_Throws_ResourceNotFoundException_WhenEquipmentNotFound() {
+
+        when(equipmentRepository.findById(anyLong())).thenReturn(Optional.empty());
 
         ResourceNotFoundException resourceNotFoundException = Assertions
                 .assertThrows(ResourceNotFoundException.class,
@@ -105,7 +102,9 @@ class EquipmentServiceTest {
 
     @Test
     void save_Return_Equipment_WhenSuccessful() {
-        EquipmentPostRequestDTO equipment = EquipmentPostRequestDTO.builder()
+        Equipment equipment = createEquipment();
+
+        EquipmentPostRequestDTO equipmentRequestDTO = EquipmentPostRequestDTO.builder()
                 .name("Aspire-3")
                 .brand("ACER")
                 .processor("Intel i5")
@@ -114,9 +113,12 @@ class EquipmentServiceTest {
                 .tombo("125478")
                 .build();
 
-        Equipment equipmentSaved = equipmentService.save(equipment);
+        when(equipmentRepository.save(any(Equipment.class))).thenReturn(equipment);
+
+        Equipment equipmentSaved = equipmentService.save(equipmentRequestDTO);
 
         Assertions.assertNotNull(equipmentSaved.getId());
+        Assertions.assertEquals(equipment.getId(), equipmentSaved.getId());
         Assertions.assertEquals(equipment.getName(), equipmentSaved.getName());
         Assertions.assertEquals(equipment.getTombo(), equipmentSaved.getTombo());
         Assertions.assertEquals(equipment.getProcessor(), equipmentSaved.getProcessor());
@@ -124,18 +126,16 @@ class EquipmentServiceTest {
         Assertions.assertEquals(equipment.getRamMemory(), equipmentSaved.getRamMemory());
         Assertions.assertEquals(equipment.getStorageMemory(), equipmentSaved.getStorageMemory());
 
+        Equipment equipmentTwo = createEquipment();
+        equipmentTwo.setId(null);
+
+        verify(equipmentRepository).save(equipmentTwo);
+        verifyNoMoreInteractions(equipmentRepository);
     }
 
     @Test
     void update_Return_Equipment_WhenSuccessful() {
-        EquipmentPostRequestDTO equipment = EquipmentPostRequestDTO.builder()
-                .name("Aspire-3")
-                .brand("ACER")
-                .processor("Intel i5")
-                .ramMemory("8 gb")
-                .storageMemory("500 mb ssd")
-                .tombo("125478")
-                .build();
+        Equipment equipmentToUpdated = createEquipment();
 
         EquipmentPutRequestDTO equipmentPutRequest = EquipmentPutRequestDTO.builder()
                 .name("Aspire-5")
@@ -146,39 +146,66 @@ class EquipmentServiceTest {
                 .tombo("125478")
                 .build();
 
-        Equipment equipmentSaved = equipmentService.save(equipment);
+        equipmentToUpdated.setName("Aspire-5");
+        equipmentToUpdated.setProcessor("Intel i3");
+        equipmentToUpdated.setRamMemory("16 gb");
+
+        when(equipmentRepository.save(any(Equipment.class))).thenReturn(equipmentToUpdated);
+        when(equipmentRepository.findById(anyLong())).thenReturn(Optional.of(equipmentToUpdated));
+
         Equipment equipmentUpdated = equipmentService.update(1L, equipmentPutRequest);
 
-        Assertions.assertEquals(equipmentSaved.getId(), equipmentUpdated.getId());
-        Assertions.assertEquals(equipmentSaved.getBrand(), equipmentUpdated.getBrand());
-        Assertions.assertNotEquals(equipmentSaved.getName(), equipmentUpdated.getName());
+        Assertions.assertEquals(equipmentToUpdated.getId(), equipmentUpdated.getId());
+        Assertions.assertEquals(equipmentToUpdated.getBrand(), equipmentUpdated.getBrand());
+        Assertions.assertEquals(equipmentToUpdated.getName(), equipmentUpdated.getName());
+        Assertions.assertEquals(equipmentToUpdated.getRamMemory(), equipmentUpdated.getRamMemory());
 
+        verify(equipmentRepository).findById(1L);
+        verify(equipmentRepository).save(equipmentToUpdated);
+        verifyNoMoreInteractions(equipmentRepository);
     }
 
     @Test
     void updateEquipmentStatus_Return_Equipment_WhenSuccessful() {
+        Equipment equipment = createEquipment();
+
         EquipmentStatusPatchRequestDTO equipmentStatusPatchRequest =
                 new EquipmentStatusPatchRequestDTO(EquipmentAvailabilityStatus.BUSY);
 
-        EquipmentPostRequestDTO equipment = EquipmentPostRequestDTO.builder()
+
+        when(equipmentRepository.findById(anyLong())).thenReturn(Optional.of(equipment));
+        when(equipmentRepository.save(any(Equipment.class))).thenReturn(equipment);
+
+
+        Equipment equipmentUpdated = equipmentService.updateEquipmentStatus(1L, equipmentStatusPatchRequest);
+
+        Assertions.assertEquals(equipment.getId(), equipmentUpdated.getId());
+        Assertions.assertEquals(EquipmentAvailabilityStatus.BUSY, equipmentUpdated.getEquipmentStatus());
+
+    }
+
+    @Test
+    void delete_Equipment_WhenSuccessful() {
+
+        Equipment equipment = createEquipment();
+
+        when(equipmentRepository.findById(anyLong())).thenReturn(Optional.of(equipment));
+        doNothing().when(equipmentRepository).deleteById(anyLong());
+        equipmentService.delete(equipment.getId());
+        verify(equipmentRepository, times(1)).deleteById(equipment.getId());
+
+    }
+
+    private Equipment createEquipment() {
+        return Equipment.builder()
+                .id(1L)
                 .name("Aspire-3")
                 .brand("ACER")
                 .processor("Intel i5")
                 .ramMemory("8 gb")
                 .storageMemory("500 mb ssd")
                 .tombo("125478")
+                .equipmentStatus(EquipmentAvailabilityStatus.AVAILABLE)
                 .build();
-
-        Equipment equipmentSaved = equipmentService.save(equipment);
-
-        Equipment equipmentUpdated = equipmentService.updateEquipmentStatus(1L, equipmentStatusPatchRequest);
-
-        Assertions.assertEquals(equipmentSaved.getId(), equipmentUpdated.getId());
-        Assertions.assertEquals(EquipmentAvailabilityStatus.BUSY, equipmentUpdated.getEquipmentStatus());
-
-    }
-
-    @Test
-    void delete() {
     }
 }
