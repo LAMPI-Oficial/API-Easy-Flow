52b50c1967885fd089cb977d2e371f299fc7ad13
Lucas Farias <lucasfariasti0101@gmail.com>
2023-05-23 20:37:40 -0300

    Adicionado cenarios de teste para salvar uma solicitação, buscar pelo id-equipamento, data de inicio e fim

--- src/test/java/br/com/ifce/easyflow/service/equipment/SolicitationServiceTest.java
+++ src/test/java/br/com/ifce/easyflow/service/equipment/SolicitationServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.ifce.easyflow.service.equipment;
 
+import br.com.ifce.easyflow.controller.dto.solicitation.SolicitationPostRequestDTO;
 import br.com.ifce.easyflow.exception.PersonNotFoundException;
 import br.com.ifce.easyflow.model.Equipment;
 import br.com.ifce.easyflow.model.Person;
@@ -10,6 +11,7 @@ import br.com.ifce.easyflow.repository.EquipmentRepository;
 import br.com.ifce.easyflow.repository.PersonRepository;
 import br.com.ifce.easyflow.repository.SolicitationRepository;
 import br.com.ifce.easyflow.service.SolicitationService;
+import br.com.ifce.easyflow.service.exceptions.BadRequestException;
 import br.com.ifce.easyflow.service.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -159,19 +162,50 @@ class SolicitationServiceTest {
     @Test
     void findByEquipmentId_Return_PageOfEquipmentsSolicitations_ByEquipmentId_WhenSuccessful() {
 
+        PageRequest pageable = PageRequest.of(0, 5);
+        List<Solicitation> solicitations = List.of(createSolicitation());
+        PageImpl<Solicitation> solicitationPage = new PageImpl<>(solicitations);
+
+        when(equipmentRepository.existsById(anyLong())).thenReturn(true);
+        when(solicitationRepository.findAllByEquipmentId(anyLong(), any(Pageable.class))).thenReturn(solicitationPage);
+
+        Page<Solicitation> solicitationsSavedPage = solicitationService.findByEquipmentId(1L, pageable);
+        List<Solicitation> solicitationList = solicitationsSavedPage.stream().toList();
+
+        Assertions.assertEquals(solicitations.get(0).getEquipment(), solicitationList.get(0).getEquipment());
+        Assertions.assertEquals(solicitations.get(0).getId(), solicitationList.get(0).getId());
+        Assertions.assertEquals(solicitations.get(0).getStatus(), solicitationList.get(0).getStatus());
     }
 
     @Test
-    void findAllByStartDate_Return_PageOfEquipmentsSolicitations_ByEquipmentId_WhenSuccessful() {
+    void findByEquipmentId_Throws_ResourceNotFoundException_WhenEquipmentNotFound() {
+
+        PageRequest pageable = PageRequest.of(0, 5);
+
+        when(equipmentRepository.existsById(anyLong())).thenReturn(false);
+
+        ResourceNotFoundException resourceNotFoundException = Assertions
+                .assertThrows(ResourceNotFoundException.class,
+                        () -> solicitationService.findByEquipmentId(1L, pageable));
+
+        Assertions.assertTrue(resourceNotFoundException
+                .getMessage()
+                .contains("The equipment was not found in the database, please check the registered equipment."));
+
+        verify(equipmentRepository).existsById(1L);
+        verifyNoMoreInteractions(equipmentRepository);
+        verifyNoInteractions(solicitationRepository);
+    }
+
+    @Test
+    void findAllByStartDate_Return_PageOfEquipmentsSolicitations_ByStartDate_WhenSuccessful() {
 
         PageRequest pageable = PageRequest.of(0, 5);
         List<Solicitation> solicitations = List.of(createSolicitation());
 
         PageImpl<Solicitation> solicitationPage = new PageImpl<>(solicitations);
 
-        LocalDate localDate = LocalDate.parse("2023-11-12", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-        when(solicitationRepository.findAllByStartDate(localDate, pageable))
+        when(solicitationRepository.findAllByStartDate(any(LocalDate.class), any(Pageable.class)))
                 .thenReturn(solicitationPage);
 
         String date = "2023-11-12";
@@ -186,17 +220,157 @@ class SolicitationServiceTest {
         Assertions.assertEquals(solicitations.get(0).getEndDate(), solicitationsSavedList.get(0).getEndDate());
         Assertions.assertEquals(solicitations.get(0).getPerson(), solicitationsSavedList.get(0).getPerson());
 
-        verify(solicitationRepository).findAllByStartDate(localDate, pageable);
+        verify(solicitationRepository).findAllByStartDate(any(LocalDate.class), any(Pageable.class));
         verifyNoMoreInteractions(solicitationRepository);
 
     }
 
     @Test
-    void findAllByEndDate() {
+    void findAllByStartDate_Throws_BadRequestException_ByStartDate_WhenDateIsInWrongFormat() {
+
+        PageRequest pageable = PageRequest.of(0, 5);
+
+        String date = "2023:11:12";
+
+        BadRequestException badRequestException = Assertions
+                .assertThrows(BadRequestException.class,
+                        () -> solicitationService.findAllByStartDate(date, pageable));
+
+        Assertions.assertTrue(badRequestException
+                .getMessage()
+                .contains("The date format does not conform to the format: yyyy-MM-dd. "));
+
+        verifyNoInteractions(solicitationRepository);
+
     }
 
     @Test
-    void save() {
+    void findAllByEndDate_Return_PageOfEquipmentsSolicitations_ByEndDate_WhenSuccessful() {
+
+        PageRequest pageable = PageRequest.of(0, 5);
+        List<Solicitation> solicitations = List.of(createSolicitation());
+
+        PageImpl<Solicitation> solicitationPage = new PageImpl<>(solicitations);
+
+        when(solicitationRepository.findAllByEndDate(any(LocalDate.class), any(Pageable.class)))
+                .thenReturn(solicitationPage);
+
+        String date = "2023-11-16";
+
+        Page<Solicitation> solicitationsPageResponse = solicitationService.findAllByEndDate(date, pageable);
+        List<Solicitation> solicitationsSavedList = solicitationsPageResponse.stream().toList();
+
+        Assertions.assertFalse(solicitationsSavedList.isEmpty());
+        Assertions.assertEquals(solicitations.get(0).getId(), solicitationsSavedList.get(0).getId());
+        Assertions.assertEquals(solicitations.get(0).getEquipment(), solicitationsSavedList.get(0).getEquipment());
+        Assertions.assertEquals(solicitations.get(0).getStartDate(), solicitationsSavedList.get(0).getStartDate());
+        Assertions.assertEquals(solicitations.get(0).getEndDate(), solicitationsSavedList.get(0).getEndDate());
+        Assertions.assertEquals(solicitations.get(0).getPerson(), solicitationsSavedList.get(0).getPerson());
+
+        verify(solicitationRepository).findAllByEndDate(any(LocalDate.class), any(Pageable.class));
+        verifyNoMoreInteractions(solicitationRepository);
+
+    }
+
+    @Test
+    void findAllByEndDate_Throws_BadRequestException_ByEndDate_WhenDateIsInWrongFormat() {
+
+        PageRequest pageable = PageRequest.of(0, 5);
+
+        String date = "23-11-12";
+
+        BadRequestException badRequestException = Assertions
+                .assertThrows(BadRequestException.class,
+                        () -> solicitationService.findAllByEndDate(date, pageable));
+
+        Assertions.assertTrue(badRequestException
+                .getMessage()
+                .contains("The date format does not conform to the format: yyyy-MM-dd. "));
+
+        verifyNoInteractions(solicitationRepository);
+
+    }
+
+
+    @Test
+    void save_Return_EquipmentSolicitation_WhenSuccessful() {
+
+        Person person = createPerson();
+        Solicitation solicitation = createSolicitation();
+        solicitation.setEquipment(null);
+
+        SolicitationPostRequestDTO requestDTO = SolicitationPostRequestDTO.builder()
+                .personId(1L)
+                .justification("Justification")
+                .startDate("2023-11-12")
+                .endDate("2023-11-16")
+                .build();
+
+        when(personRepository.findById(anyLong())).thenReturn(Optional.of(person));
+        when(solicitationRepository.save(any(Solicitation.class))).thenReturn(solicitation);
+
+        Solicitation saved = solicitationService.save(requestDTO);
+
+        Assertions.assertNotNull(saved.getId());
+        Assertions.assertEquals(requestDTO.getPersonId(), saved.getPerson().getId());
+        Assertions.assertEquals(requestDTO.getStartDate(), saved.getStartDate().toString());
+        Assertions.assertEquals(requestDTO.getEndDate(), saved.getEndDate().toString());
+
+        verify(solicitationRepository, times(1)).save(any(Solicitation.class));
+        verifyNoMoreInteractions(solicitationRepository);
+        verify(personRepository).findById(anyLong());
+
+    }
+
+    @Test
+    void save_Throw_PersonNotFondException_WhenPersonNotFound() {
+
+        when(personRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        SolicitationPostRequestDTO requestDTO = SolicitationPostRequestDTO.builder()
+                .personId(1L)
+                .justification("Justification")
+                .startDate("23-11-12")
+                .endDate("2023:11:16")
+                .build();
+
+        PersonNotFoundException personNotFoundException = Assertions.assertThrows(PersonNotFoundException.class,
+                () -> solicitationService.save(requestDTO));
+
+
+        Assertions.assertTrue(personNotFoundException
+                .getMessage()
+                .contains("The person was not found in the database, please check the registered persons."));
+
+        verify(personRepository).findById(anyLong());
+        verifyNoInteractions(solicitationRepository);
+
+    }
+
+    @Test
+    void save_Throw_BadRequestException_WhenDateIsInWrongFormat() {
+
+        Person person = createPerson();
+
+        when(personRepository.findById(anyLong())).thenReturn(Optional.of(person));
+
+        SolicitationPostRequestDTO requestDTO = SolicitationPostRequestDTO.builder()
+                .personId(1L)
+                .justification("Justification")
+                .startDate("23-11-12")
+                .endDate("2023:11:16")
+                .build();
+
+        BadRequestException badRequestException = Assertions.assertThrows(BadRequestException.class,
+                () -> solicitationService.save(requestDTO));
+
+
+        Assertions.assertTrue(badRequestException
+                .getMessage()
+                .contains("The date format does not conform to the format: yyyy-MM-dd. "));
+
+        verifyNoInteractions(solicitationRepository);
+
     }
 
     @Test
