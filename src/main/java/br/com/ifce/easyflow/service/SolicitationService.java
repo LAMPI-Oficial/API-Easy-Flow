0261916853470766ae9850d557af687913eefe35
Lucas Farias <lucasfariasti0101@gmail.com>
2023-05-24 14:17:20 -0300

    fix(SolicitationService): muda a lógica de verificação para reprovação de uma solicitação e adiciona verificação de valor null no delete

--- src/main/java/br/com/ifce/easyflow/service/SolicitationService.java
+++ src/main/java/br/com/ifce/easyflow/service/SolicitationService.java
@@ -12,7 +12,6 @@ import br.com.ifce.easyflow.model.enums.SolicitationStatus;
 import br.com.ifce.easyflow.repository.EquipmentRepository;
 import br.com.ifce.easyflow.repository.PersonRepository;
 import br.com.ifce.easyflow.repository.SolicitationRepository;
-//import br.com.ifce.easyflow.service.exceptions.BadRequestException;
 import br.com.ifce.easyflow.service.exceptions.BadRequestException;
 import br.com.ifce.easyflow.service.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -158,26 +157,27 @@ public class SolicitationService {
     public void denySolicitation(Long id) {
 
         Solicitation solicitation = this.findById(id);
-        if (solicitation.getStatus().equals(SolicitationStatus.PENDING)) {
-            solicitation.setStatus(SolicitationStatus.DENIED);
-            solicitationRepository.save(solicitation);
+        if (!solicitation.getStatus().equals(SolicitationStatus.PENDING)) {
+            throw new BadRequestException("Request must be pending to disapprove it");
         }
-        throw new BadRequestException("Request must be pending to disapprove it");
+        solicitation.setStatus(SolicitationStatus.DENIED);
+        solicitationRepository.save(solicitation);
     }
 
     @Transactional
     public void delete(Long id) {
 
         Solicitation solicitation = this.findById(id);
-        Long equipmentId = solicitation.getEquipment().getId();
 
-        Optional<Equipment> equipment = equipmentRepository.findById(equipmentId);
+        if (solicitation.getEquipment() != null) {
 
-        if (equipment.isPresent()) {
+            Optional<Equipment> equipment = equipmentRepository.findById(solicitation.getEquipment().getId());
 
-            equipment.get().setEquipmentStatus(EquipmentAvailabilityStatus.AVAILABLE);
-            equipmentRepository.save(equipment.get());
-            solicitationRepository.deleteById(id);
+            if (equipment.isPresent()) {
+                equipment.get().setEquipmentStatus(EquipmentAvailabilityStatus.AVAILABLE);
+                equipmentRepository.save(equipment.get());
+                solicitationRepository.deleteById(id);
+            }
 
         } else {
             solicitationRepository.deleteById(id);
